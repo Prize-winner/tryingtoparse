08797c6103e60b132518a388c8fd80e15d9bcea3
Prize-winner <3220016928@qq.com>
2019-06-15 00:54:56 +0800

    视图解析_thymeleaf

---
+++ src/main/java/com/haicao/TryingtoparseApplication.java
@@ -0,0 +1,13 @@
+package com.haicao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryingtoparseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryingtoparseApplication.class, args);
+    }
+
+}
